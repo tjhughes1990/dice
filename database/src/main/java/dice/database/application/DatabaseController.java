4ccbb272066a29282c7e5c988c2ebf9a03dfe05b
tjhughes1990 <tjhughes1990@gmail.com>
2020-03-09 23:49:54 +0000

    #5 Added REST call to database service, and updated docker configs.

--- database/src/main/java/dice/database/application/DatabaseController.java
+++ database/src/main/java/dice/database/application/DatabaseController.java
@@ -1,31 +1,72 @@
 package dice.database.application;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import dice.common.DiceException;
 import dice.common.types.DiceRollType;
+import dice.database.connection.DatabaseConnector;
 
 @RestController
 public class DatabaseController {
 
+    private static final Log LOG = LogFactory.getLog(DatabaseController.class);
+
+    private static final String DB_PROPS_PATH = "database.env";
+    private static final DatabaseConnector DB_CONNECTOR = new DatabaseConnector(DB_PROPS_PATH);
+
+    private final Connection connection;
+
     /**
      * Constructor.
+     *
+     * @throws DiceException
+     *             if a connection to the database could not be established.
      */
-    public DatabaseController() {
+    public DatabaseController() throws DiceException {
+        connection = DB_CONNECTOR.connect();
     }
 
-    @PostMapping
+    @PostMapping(value = "saveDice",
+                 consumes = MediaType.APPLICATION_JSON_VALUE,
+                 produces = MediaType.APPLICATION_JSON_VALUE)
     public void saveDice(final List<DiceRollType> diceList) {
         // TODO
+        final String sql = "";
+        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
+            System.out.println("SAVE DICE");
+        } catch (final SQLException e) {
+            final String errMsg = "Failed to save dice collection to database";
+            LOG.error(errMsg, e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, errMsg, e);
+        }
     }
 
-    @GetMapping
-    public List<DiceRollType> loadDice() {
+    @GetMapping(value = "loadDice", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<DiceRollType> loadDice(@RequestParam(value = "id", required = true) final long id) {
         // TODO
+        final String sql = "";
+        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
+            // TODO
+            System.out.println("LOAD DICE");
+        } catch (final SQLException e) {
+            final String errMsg = "Failed to load dice collection from database";
+            LOG.error(errMsg, e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, errMsg, e);
+        }
         return Collections.emptyList();
     }
 }
--- service/src/main/java/dice/service/application/DiceController.java
+++ service/src/main/java/dice/service/application/DiceController.java
@@ -1,12 +1,23 @@
 package dice.service.application;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import javax.net.ssl.SSLContext;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +30,10 @@ import dice.service.roll.RollAggregator;
 @RestController
 public class DiceController {
 
+    private static final String DB_URL_ROOT = "http://db:8081/";
+    private static final String DB_LOAD_URL = DB_URL_ROOT + "loadDice?id=";
+
+    private final HttpClient httpClient;
     private final DiceRoller diceRoller;
 
     /**
@@ -28,6 +43,11 @@ public class DiceController {
      *             if the {@link DiceRoller} could not be created.
      */
     public DiceController() throws DiceException {
+        try {
+            httpClient = HttpClient.newBuilder().sslContext(SSLContext.getDefault()).build();
+        } catch (final NoSuchAlgorithmException e) {
+            throw new DiceException("Failed to create HTTP client", e);
+        }
         diceRoller = new DiceRoller();
     }
 
@@ -37,12 +57,14 @@ public class DiceController {
      * @param diceRolls
      *            the dice rolls to be performed.
      *
-     * @return the dice rolls, with rolls perfomed.
+     * @return the dice rolls, with rolls performed.
      *
      * @throws DiceException
      *             if the dice definitions were invalid.
      */
-    @PostMapping(value = "/roll", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/roll",
+                 consumes = MediaType.APPLICATION_JSON_VALUE,
+                 produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody List<DiceRollType> roll(@RequestBody final List<DiceRollType> diceRolls) throws DiceException {
         final RollAggregator aggregator = new RollAggregator();
         diceRolls.stream().forEach(d -> aggregator.addDiceRoll(d));
@@ -59,7 +81,9 @@ public class DiceController {
      *
      * @return {@code true} iff the dice collection was successfully saved to the database.
      */
-    @PostMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/save",
+                 consumes = MediaType.APPLICATION_JSON_VALUE,
+                 produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody boolean save(@RequestBody final DiceRollCollection diceRollCollection) {
         // TODO
         return false;
@@ -72,10 +96,38 @@ public class DiceController {
      *            the ID of the collection to load.
      *
      * @return a list of dice in the collection.
+     *
+     * @throws DiceException
+     *             if no response was received from the database.
      */
-    @GetMapping(value = "load")
-    public @ResponseBody List<DiceRollType> load(final long collectionId) {
+    @GetMapping(value = "load", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody List<DiceRollType> load(@RequestParam(value = "id", required = true) final long collectionId)
+            throws DiceException {
+
+        final URI uri = URI.create(DB_LOAD_URL + collectionId);
+        final HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
+        sendRequest(request);
+
         // TODO
         return Collections.emptyList();
+    }
+
+    /**
+     * Send and await a REST request.
+     *
+     * @param request
+     *            the request to send.
+     *
+     * @return the body of the response.
+     *
+     * @throws DiceException
+     *             if the request timed out.
+     */
+    private byte[] sendRequest(final HttpRequest request) throws DiceException {
+        try {
+            return httpClient.sendAsync(request, BodyHandlers.ofByteArray()).get(10, TimeUnit.SECONDS).body();
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            throw new DiceException("Response not received from REST request", e);
+        }
     }
 }
