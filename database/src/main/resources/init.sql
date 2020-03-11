4b49a8f9f314a132e1b9ab6fd992cecd2f75e559
tjhughes1990 <tjhughes1990@gmail.com>
2020-03-11 23:37:43 +0000

    #5 Added delete REST method.

--- database/src/main/resources/init.sql
+++ database/src/main/resources/init.sql
@@ -1,25 +1,29 @@
 CREATE SCHEMA dice;
 
 CREATE TABLE dice.dice_collection (
-    id bigint PRIMARY KEY,
-    name text NOT NULL UNIQUE
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE dice.dice (
-    id bigint NOT NULL UNIQUE,
-    collection_id bigint NOT NULL,
-    min_result int NOT NULL,
-    max_result int NOT NULL,
-    roll_number int NOT NULL
+    id BIGSERIAL PRIMARY KEY,
+    collection_id BIGINT NOT NULL,
+    min_result INT NOT NULL,
+    max_result INT NOT NULL,
+    roll_number INT NOT NULL,
+    constraint fk_dice_id
+        FOREIGN KEY (collection_id)
+        REFERENCES dice.dice_collection(id)
+        ON DELETE CASCADE
 );
 
 -- Add example dice collection
-INSERT INTO dice.dice_collection(id, name)
+INSERT INTO dice.dice_collection(name)
     VALUES
-        (1, 'D6 x3'),
-        (2, 'D10 & D12');
-INSERT INTO dice.dice(id, collection_id, min_result, max_result, roll_number)
+        ('D6 x3'),
+        ('D10 & D12');
+INSERT INTO dice.dice(collection_id, min_result, max_result, roll_number)
     VALUES
-        (1, 1, 1, 6, 3),
-        (2, 2, 1, 10, 1),
-        (3, 2, 1, 12, 1);
+        (1, 1, 6, 3),
+        (2, 1, 10, 1),
+        (2, 1, 12, 1);
